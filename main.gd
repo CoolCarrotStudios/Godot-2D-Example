972f84e54ffdabe2e778ed79cb4928cf96a05223
Cool Carrot Studios <>
2023-05-29 00:29:02 +0100

    HUD Complete. Up to "Remove Old Creeps" in the tutorial

--- main.gd
+++ main.gd
@@ -2,10 +2,6 @@ extends Node
 
 @export var mob_scene: PackedScene
 var score
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	new_game()
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -15,8 +11,11 @@ func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+
 func game_over():
-	pass # Replace with function body.
+	$HUD.show_game_over()
 
 
 func _on_mob_timer_timeout():
@@ -40,6 +39,7 @@ func _on_mob_timer_timeout():
 
 func _on_score_timer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 
 func _on_start_timer_timeout():
