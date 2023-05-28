6bf5b5e8b53702b9fde2a3b754c7c882d27d4405
Cool Carrot Studios <>
2023-05-28 23:36:15 +0100

    Added Enemy Functionality.

--- player.gd
+++ player.gd
@@ -1,11 +1,18 @@
 extends Area2D
 
+signal hit
+
 @export var speed = 400
 var screen_size
 
 func _ready():
 	screen_size = get_viewport_rect().size
 	hide()
+
+func _start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
 
 func _process(delta):
 	var velocity = Vector2.ZERO
@@ -35,3 +42,9 @@ func _process(delta):
 	elif velocity.y != 0:
 		$AnimatedSprite2D.animation = "up"
 		$AnimatedSprite2D.flip_v = velocity.y > 0
+
+
+func _on_body_entered(body):
+	hide()
+	hit.emit()
+	$CollisionShape2D.set_deferred("disabled",true)
